1df79c6afffa2a03afd694e996257a187d7778e0
Pena-Martinez <Rocio.PenaMartinez@austintexas.gov>
2021-05-15 19:50:18 -0500

    how tf do i get express methods to do their thing? can't find syntax for more than just sending resp types. how do i get the server to work on my array obj w/ these methods? only first method is working when checked on postman.

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const express = require('express');
+const app = express();
 
-const { users } = require('./state')
+const port = process.env.PORT || 4000 //means: whatever is in var PORT, or 4000 if nothing there. nec to be able to .listen to port w/o hard coding?
 
-/* BEGIN - create routes here */
+const { users } = require('./state');//pulling in [users] from state.js
+
+//GET /users
+app.get('/users', function(req, res){
+  res.send(users);
+});
+//[users] shows up in postman w/ get to /users
 
 
-/* END - create routes here */
+// GET /users/1
+app.get('/users/1', function(req, res){  //not hard coded = '/users/:id',
+  res.send(req.params.id);//i want to send user of _id:1
+});
+//cant get it to send object at _id=1 :/ can only send id itself
+
+// POST /users
+app.post('/users', function(req, res, next){
+  {
+     "_id": 6,
+     "name": "Guy Duderson",
+     "occupation": "Surfer",
+     "avatar": "https://www.surfertoday.com/images/celebrities/matthewmcconaughey_surfing.jpg"
+  }
+  next({
+    res.json(); //i want to send user of _id:6 
+  })
+});
+
+
+
+/* PUT /users/1
+app.put('/users/1', function (req, res, next) {
+  const keyVal = req.key
+  wtf how do we actually do middleware w/ these methods that doesn't just send a text response 
+  next(res.json(users/1))
+})
+Give your server the ability to respond to a PUT request with a path "/users/1" and just change any key value (ex. name, occupation) on the first user object in the users array in state.js.
+ Use `res.json()` to send this user back to the client.
+
+* DELETE /users/1
+app.delete('/users/1', function (req, res, next) {
+  res.send('users array without users/1')
+  next(res.send('deleted'));
+})
+* Give your server the ability to respond to a DELETE request with a path "/users/1" and remove the first item from the users array. Use `res.send()` to send back a messsage, "deleted"*/
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
